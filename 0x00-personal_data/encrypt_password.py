bad0252d81eae38844a0c486c471db43b7aaeb5d
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-02-08 12:53:58 +0100

    7th task is done

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,10 @@ def hash_password(password: str) -> bytes:
         Function that encrypts the passwords with generated salt
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+        Function that checks the encryped password and the real one
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
