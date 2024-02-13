2b1a185a543b118bfff97c43763d446582923b85
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-02-13 13:51:02 +0100

    11th task is done

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -60,3 +60,21 @@ class BasicAuth(Auth):
         email = decoded_base64_authorization_header.split(':')[0]
         password = decoded_base64_authorization_header.split(':')[1]
         return (email, password)
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str
+            ) -> TypeVar('User'):  # type: ignore
+        """ Method that returns the User based on its email and password
+        """
+        if not user_email or type(user_email) is not str:
+            return None
+        if not user_pwd or type(user_pwd) is not str:
+            return None
+
+        users = User.search({'email': user_email})
+        if not users:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
