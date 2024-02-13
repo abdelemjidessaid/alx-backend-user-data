4f2fc177d26985259e7081af057969fde7ee900d
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-02-13 11:06:20 +0100

    4th task is done

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Module of authentication
+"""
+from flask import request
+from typing import List, TypeVar
+from models.user import User
+
+
+class Auth:
+    """ Class of authentication
+    """
+
+    def __init__(self) -> None:
+        """ Class constructure
+        """
+        pass
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Method that require the authentication
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Method that check the authorizations
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'): # type: ignore
+        """ Method that returns the user
+        """
+        return None
