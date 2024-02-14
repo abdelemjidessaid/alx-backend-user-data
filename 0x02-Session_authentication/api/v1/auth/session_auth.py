37be41c8c714b805e74be701b2c90e181e4bc41d
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-02-14 17:08:35 +0100

    3rd task is done

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -11,12 +11,12 @@ from api.v1.auth.auth import Auth
 class SessionAuth(Auth):
     """ Class SessionAuth that inherits from Auth
     """
+    user_id_by_session_id = {}
 
     def __init__(self) -> None:
         """ SessionAuth constructure calls the super constructure
         """
         super().__init__()
-        self.user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
         """ Function that creates new session
@@ -24,5 +24,5 @@ class SessionAuth(Auth):
         if not user_id or not isinstance(user_id, str):
             return None
         id = str(uuid4())
-        self.user_id_by_session_id[id] = user_id
+        SessionAuth.user_id_by_session_id[id] = user_id
         return id
