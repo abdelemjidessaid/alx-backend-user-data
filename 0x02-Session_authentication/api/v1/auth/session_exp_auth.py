d71ac5a957ac3e66b8bfe966b16dd3e032c12e2f
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-02-15 15:13:33 +0100

    101st task is done

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -2,7 +2,7 @@
 """ Module of Session expiration authentication
 """
 from os import environ
-from datetime import datetime
+from datetime import timedelta, datetime
 from flask import request, abort
 from api.v1.auth.session_auth import SessionAuth
 
@@ -28,7 +28,7 @@ class SessionExpAuth(SessionAuth):
         session_id = super().create_session(user_id)
         if not session_id:
             return None
-        self.user_id_by_session_id[session_id] = {
+        SessionAuth.user_id_by_session_id[session_id] = {
             'user_id': user_id,
             'created_at': datetime.now()
         }
@@ -39,7 +39,7 @@ class SessionExpAuth(SessionAuth):
         """
         if not session_id:
             return None
-        session = self.user_id_by_session_id.get(session_id)
+        session = SessionExpAuth.user_id_by_session_id.get(session_id)
         if not session:
             return None
         if self.session_duration <= 0:
@@ -47,7 +47,7 @@ class SessionExpAuth(SessionAuth):
         created_at = session.get('created_at')
         if not created_at:
             return None
-        now = datetime.now()
-        if (created_at + self.session_duration) < now:
+        delta = timedelta(seconds=self.session_duration)
+        if (created_at + delta) < datetime.now():
             return None
         return session.get('user_id')
