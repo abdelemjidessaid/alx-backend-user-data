df6dcf8854c3feea0935a968a7590bda1194afe4
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-02-15 13:26:53 +0100

    8th task is done

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -2,21 +2,21 @@
 """ Module of Session Authentication view
 """
 from flask import jsonify, abort, request
-import os
+from os import getenv
 from api.v1.views import app_views
 from models.user import User
 
 
-@app_views.route('/auth_session/login', methods=['POST'],
-                 strict_slashes=False)
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
-    """ Function that retrieve the login info (Email, Password)
-        from request form
+    """ POST /auth_session/login
+    Return:
+     - User instance based on email
     """
     email = request.form.get('email')
+    password = request.form.get('password')
     if not email:
         return jsonify({"error": "email missing"}), 400
-    password = request.form.get('password')
     if not password:
         return jsonify({"error": "password missing"}), 400
     try:
@@ -25,12 +25,12 @@ def login():
         return jsonify({"error": "no user found for this email"}), 404
     if not users:
         return jsonify({"error": "no user found for this email"}), 404
-    for user in users:
-        if user.is_valid_password(password):
-            from api.v1.app import auth
-            session_id = auth.create_session(user.id)
-            response = jsonify(user.to_json())
-            session_name = os.environ.get('SESSION_NAME')
-            return response.set_cookie(session_name, session_id)
-        return jsonify({"error": "wrong password"}), 401
+    for u in users:
+        if not u.is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 401
+        from api.v1.app import auth
+        session_id = auth.create_session(u.id)
+        out = jsonify(u.to_json())
+        out.set_cookie(getenv('SESSION_NAME'), session_id)
+        return out
     return jsonify({"error": "no user found for this email"}), 404
