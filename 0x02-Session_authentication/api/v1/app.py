1bb17ac1d5ef24a00a97c86e7f3ac466c6198c64
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-02-16 11:15:15 +0100

    10th task is updated

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -55,18 +55,15 @@ def forbidden(error) -> str:
 def before_request() -> None:
     """ Function that run a passed function after each request
     """
-    paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-             '/api/v1/forbidden/', '/api/v1/auth_session/login/']
-    if not auth:
-        return None
-    if not auth.require_auth(request.path, paths):
-        return None
-    if not auth.authorization_header(request)\
-            and not auth.session_cookie(request):
-        abort(401)
-    if not auth.current_user(request):
-        abort(403)
-    request.current_user = auth.current_user(request)
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
+    if auth and auth.require_auth(request.path, excluded_paths):
+        if (not auth.authorization_header(request) and
+                not auth.session_cookie(request)):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
+        request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
