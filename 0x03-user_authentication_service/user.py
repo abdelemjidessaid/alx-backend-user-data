0668dc8b6dc36c2ad047ccecc080bd598086694d
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-02-19 19:15:42 +0100

    2nd task is done

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-""" Module of SQLAchemy. Creating table of Users
-"""
+""" Module of User Model """
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Integer, String, Column
 
@@ -14,8 +13,8 @@ class User(Base):
     # the table name
     __tablename__ = 'users'
     # the table columns initialization
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=False)
-    reset_token = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
