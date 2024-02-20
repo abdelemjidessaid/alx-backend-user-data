e53cead3d01ee5207415fb7a7a227db0e7dda727
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-02-20 14:51:25 +0100

    20th task is done

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -73,6 +73,19 @@ def get_reset_password_token() -> str:
     return jsonify({"email": f"{email}", "reset_token": f"{password_token}"})
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """ Route function that updates the password with new one """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token=reset_token, password=new_password)
+        return jsonify({"email": f"{email}", "message": "Password updated"})
+    except ValueError:
+        abort(403)
+
+
 if __name__ == '__main__':
     """ Entry point of the back-end program """
     app.run(host='0.0.0.0', port='5000')
