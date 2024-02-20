f48a184dce37a0b2f8f057dbeb4d77927ea96b96
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-02-20 12:23:41 +0100

    12th task is done and updated for requirement error

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -35,7 +35,7 @@ def login() -> str:
         valid_auth = AUTH.valid_login(email=email, password=password)
         if valid_auth:
             session_id = AUTH.create_session(email=email)
-            response = jsonify({"email": email, "message": "logged in"})
+            response = jsonify({"email": f'{email}', "message": "logged in"})
             response.set_cookie('session_id', session_id)
             return response
     abort(401)
