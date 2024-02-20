4f17c030b6d6de5a1cb9502347900e04f956e5da
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-02-20 11:10:13 +0100

    9th task is done

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -33,3 +33,15 @@ class Auth:
         if user:
             raise ValueError(f'User {user.email} already exists.')
         return self._db.add_user(email=email, hashed_password=hashed_password)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Method that validates login info """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        if user:
+            return bcrypt.checkpw(
+                password=password.encode('utf-8'),
+                hashed_password=user.hashed_password)
+        return False
