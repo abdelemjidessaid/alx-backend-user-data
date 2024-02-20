b7e9d6d2f4dd2ac6a96b66883821477a1d2cf261
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-02-20 12:30:30 +0100

    13th task is done

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -68,3 +68,16 @@ class Auth:
             self._db.update_user(user.id, session_id=session_id)
             return session_id
         return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """ Method that returns the user by its session id
+            Return:
+                User by its Session ID
+        """
+        if session_id:
+            try:
+                user = self._db.find_user_by(session_id=session_id)
+                return user
+            except NoResultFound:
+                return None
+        return None
