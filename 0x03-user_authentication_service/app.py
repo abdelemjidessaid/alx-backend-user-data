3cba582c488be15ca282e02ae82a1b81ef2e42cc
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-02-20 12:09:59 +0100

    12th task is updated

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -31,14 +31,16 @@ def login() -> str:
     """ Route function that log the users in """
     email = request.form.get('email')
     password = request.form.get('password')
-    valid_login = AUTH.valid_login(email, password)
-    if not valid_login:
-        abort(401)
-    session_id = AUTH.create_session(email)
-    response = jsonify({"email": f"{email}", "message": "logged in"})
-    response.set_cookie('session_id', session_id)
-    return response
+    if email and password:
+        valid_auth = AUTH.valid_login(email=email, password=password)
+        if valid_auth:
+            session_id = AUTH.create_session(email=email)
+            response = jsonify({"email": email, "message": "logged in"})
+            response.set_cookie('session_id', session_id)
+            return response
+    abort(401)
 
 
 if __name__ == '__main__':
+    """ Entry point of the back-end program """
     app.run(host='0.0.0.0', port='5000')
